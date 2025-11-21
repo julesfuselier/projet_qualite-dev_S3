9c310f49455ae74c38a43ea1497f69583ecf94ab
Jules FUSELIER <jules.fuselier@etu.univ-amu.fr>
2025-11-21 17:06:10 +0100

    Feature: Enhance character eating mechanics with food type and freshness checks

--- src/main/java/fr/amu/iut/model/items/foods/FoodFactory.java
+++ src/main/java/fr/amu/iut/model/items/foods/FoodFactory.java
@@ -5,37 +5,38 @@ public class FoodFactory {
     public Food createFood(FoodType type) {
         switch (type) {
             case FISH:
-                return new Food("Fish", 50, true, FreshnessStatus.FRESH);
+                // Ajout de 'type' (ou FoodType.FISH) en 5ème argument
+                return new Food("Fish", 50, true, FreshnessStatus.FRESH, type);
             case CLOVER:
-                return new Food("Clover", 20, true, FreshnessStatus.FRESH);
+                return new Food("Clover", 20, true, FreshnessStatus.FRESH, type);
             case WILD_BOAR:
-                return new Food("Wild Boar", 80, true, FreshnessStatus.FRESH);
+                return new Food("Wild Boar", 80, true, FreshnessStatus.FRESH, type);
             case SALT:
-                return new Food("Salt", 5, false, FreshnessStatus.FRESH);
-            case  CARROT:
-                return new Food("Carrot", 30, true, FreshnessStatus.FRESH);
+                return new Food("Salt", 5, false, FreshnessStatus.FRESH, type);
+            case CARROT:
+                return new Food("Carrot", 30, true, FreshnessStatus.FRESH, type);
             case MISTLETOE:
-                return new Food("Mistletoe", 15, true, FreshnessStatus.FRESH);
+                return new Food("Mistletoe", 15, true, FreshnessStatus.FRESH, type);
             case LOBSTER:
-                return new Food("Lobster", 70, true, FreshnessStatus.FRESH);
+                return new Food("Lobster", 70, true, FreshnessStatus.FRESH, type);
             case STRAWBERRY:
-                return new Food("Strawberry", 25, true, FreshnessStatus.FRESH);
+                return new Food("Strawberry", 25, true, FreshnessStatus.FRESH, type);
             case ROCK_OIL:
-                return new Food("Rock Oil", 10, false, FreshnessStatus.FRESH);
+                return new Food("Rock Oil", 10, false, FreshnessStatus.FRESH, type);
             case BEET_JUICE:
-                return new Food("Beet Juice", 35, true, FreshnessStatus.FRESH);
+                return new Food("Beet Juice", 35, true, FreshnessStatus.FRESH, type);
             case HONEY:
-                return new Food("Honey", 40, true, FreshnessStatus.FRESH);
+                return new Food("Honey", 40, true, FreshnessStatus.FRESH, type);
             case WINE:
-                return new Food("Wine", 60, false, FreshnessStatus.FRESH);
+                return new Food("Wine", 60, false, FreshnessStatus.FRESH, type);
             case MEAD:
-                return new Food("Mead", 55, false, FreshnessStatus.FRESH);
+                return new Food("Mead", 55, false, FreshnessStatus.FRESH, type);
             case TWO_HEADED_UNICORNN_MILK:
-                return new Food("Two-Headed Unicorn Milk", 100, true, FreshnessStatus.FRESH);
+                return new Food("Two-Headed Unicorn Milk", 100, true, FreshnessStatus.FRESH, type);
             case IDEFIX_HAIR:
-                return new Food("Idefix Hair", 5, false, FreshnessStatus.FRESH);
+                return new Food("Idefix Hair", 5, false, FreshnessStatus.FRESH, type);
             case SECRET_INGREDIENT:
-                return new Food("Secret Ingredient", 150, true, FreshnessStatus.FRESH);
+                return new Food("Secret Ingredient", 150, true, FreshnessStatus.FRESH, type);
             default:
                 throw new IllegalArgumentException("Unknown food type: " + type);
         }
