02fece347001f4a75f216c4615efdc962b888e03
Jules FUSELIER <jules.fuselier@etu.univ-amu.fr>
2025-11-21 13:40:04 +0100

    Feature: Ajout des types de nourriture dans le Factory

--- src/main/java/fr/amu/iut/model/items/foods/FoodFactory.java
+++ src/main/java/fr/amu/iut/model/items/foods/FoodFactory.java
@@ -7,6 +7,37 @@ public class FoodFactory {
             case FISH:
                 return new Food("Fish", 50, true, FreshnessStatus.FRESH);
             case CLOVER:
+                return new Food("Clover", 20, true, FreshnessStatus.FRESH);
+            case WILD_BOAR:
+                return new Food("Wild Boar", 80, true, FreshnessStatus.FRESH);
+            case SALT:
+                return new Food("Salt", 5, false, FreshnessStatus.FRESH);
+            case  CARROT:
+                return new Food("Carrot", 30, true, FreshnessStatus.FRESH);
+            case MISTLETOE:
+                return new Food("Mistletoe", 15, true, FreshnessStatus.FRESH);
+            case LOBSTER:
+                return new Food("Lobster", 70, true, FreshnessStatus.FRESH);
+            case STRAWBERRY:
+                return new Food("Strawberry", 25, true, FreshnessStatus.FRESH);
+            case ROCK_OIL:
+                return new Food("Rock Oil", 10, false, FreshnessStatus.FRESH);
+            case BEET_JUICE:
+                return new Food("Beet Juice", 35, true, FreshnessStatus.FRESH);
+            case HONEY:
+                return new Food("Honey", 40, true, FreshnessStatus.FRESH);
+            case WINE:
+                return new Food("Wine", 60, false, FreshnessStatus.FRESH);
+            case MEAD:
+                return new Food("Mead", 55, false, FreshnessStatus.FRESH);
+            case TWO_HEADED_UNICORNN_MILK:
+                return new Food("Two-Headed Unicorn Milk", 100, true, FreshnessStatus.FRESH);
+            case IDEFIX_HAIR:
+                return new Food("Idefix Hair", 5, false, FreshnessStatus.FRESH);
+            case SECRET_INGREDIENT:
+                return new Food("Secret Ingredient", 150, true, FreshnessStatus.FRESH);
+            default:
+                throw new IllegalArgumentException("Unknown food type: " + type);
         }
     }
 }
