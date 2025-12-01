2e19443d241d30da2bdc180ed88df8be72cca791
Loan <loan.allard@etu.univ-amu.fr>
2025-12-01 15:16:42 +0100

    Correction d'erreurs dans Space.java

--- src/main/java/fr/amu/iut/model/spaces/Space.java
+++ src/main/java/fr/amu/iut/model/spaces/Space.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Space {
-    private String name;
+    private final String name;
     private double surface;
     private Character leader;
-    private List<Character> characters;
-    private List<Food> foods;
+    private final List<Character> characters;
+    private final List<Food> foods;
 
     public Space(String name, double surface, Character leader) {
         this.name = name;
@@ -29,8 +29,49 @@ public abstract class Space {
         foods = new ArrayList<>();
     }
 
+    public Space(String name) {
+        this.name = name;
+        this.characters = new ArrayList<>();
+        this.foods = new ArrayList<>();
+    }
+
     public abstract boolean authorized(Character c);
 
+    // Get village name
+    public String getName() {
+        return name;
+    }
+
+    // Get characters name
+    public List<Character> getCharacters() {
+        return characters;
+    }
+
+    // Get foods
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    // Add new food
+    public void addFood(Food food) {
+        this.foods.add(food);
+    }
+
+    // Remove food
+    public void removeFood(Food food) {
+        this.foods.remove(food);
+    }
+
+    // True if location istanceof battlefield
+    public boolean isBattlefield() {
+        if (!(this instanceof Battlefield)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Add character to the space
     public boolean addCharacter(Character c) {
         if (!authorized(c)){
             return false;
@@ -39,10 +80,12 @@ public abstract class Space {
         return true;
     }
 
+    // Remove character from the space
     public void removeCharacter(Character c) {
         characters.remove(c);
     }
 
+    // Show characteristics of the space
     public void showCharacteristics(){
         System.out.println("Name: " + name);
         System.out.println("Surface : " + surface);
@@ -59,6 +102,7 @@ public abstract class Space {
         }
     }
 
+    // Heal character
     public void healCharacter(Character c, int amount){
         if(!(characters.contains(c))){
             return;
@@ -66,15 +110,14 @@ public abstract class Space {
         c.beHealed(amount);
     }
 
-    public void healFood(Character c, Food f, int amount){
+    // Faire manger un personnage
+    public void eatFood(Character c, Food f){
         if(!(foods.contains(f))){
             return;
         } else if (!(characters.contains(c))) {
             return;
         }
-        c.eat(amount);
+        c.eat(f);
         foods.remove(f);
     }
-
-
 }
