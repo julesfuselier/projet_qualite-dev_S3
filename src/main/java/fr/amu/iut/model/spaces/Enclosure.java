bb057c7c1aa9768b08e45d0060c1794a71c80937
Loan ALLARD <loan.allard@etu.univ-amu.fr>
2025-12-01 13:47:12 +0100

    Lieux -> dev (#3)
    * abstract class Space
    
    * Différents lieux avec les autorisations ou non d'entrer
    
    ---------
    
    Co-authored-by: ambreROFFIDAL <ambre.roffidal@etu.univ-amu.fr>

---
+++ src/main/java/fr/amu/iut/model/spaces/Enclosure.java
@@ -0,0 +1,15 @@
+package fr.amu.iut.model.spaces;
+
+import fr.amu.iut.model.characters.Character;
+import fr.amu.iut.model.characters.Lycanthrope;
+
+public class Enclosure extends Space {
+    public Enclosure(String name, double surface) {
+        super(name, surface);
+    }
+
+    @Override
+    public boolean authorized(Character c){
+        return c instanceof Lycanthrope;
+    }
+}
