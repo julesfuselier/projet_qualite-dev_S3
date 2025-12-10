5df395f77e8f18ddddc3940223128b8d1d56ddf2
Loan <loan.allard@etu.univ-amu.fr>
2025-12-10 23:50:03 +0100

    Création d'une classe AlphaCouple et ajout des méthodes nécessaires

---
+++ src/main/java/fr/amu/iut/model/lycanthropes/AlphaCouple.java
@@ -0,0 +1,66 @@
+package fr.amu.iut.model.lycanthropes;
+
+import fr.amu.iut.model.characters.jobs.Lycanthrope;
+import java.util.Random;
+
+public class AlphaCouple {
+
+    private Lycanthrope male;
+    private Lycanthrope female;
+    private Pack pack;
+
+    // Constructeur
+    public AlphaCouple(Lycanthrope male, Lycanthrope female, Pack pack) {
+        if (male.getSex() != 'M' || female.getSex() != 'F') {
+            throw new IllegalArgumentException("Le couple Alpha doit être composé d'un mâle et d'une femelle.");
+        }
+        this.male = male;
+        this.female = female;
+        this.pack = pack;
+    }
+
+    // Affiche les charactéristiques du couple
+    public void displayCharacteristics() {
+        System.out.println("Couple Alpha : ");
+        System.out.println("\nMâle :");
+        male.printCharacteristics();
+        System.out.println("\nFemelle :");
+        female.printCharacteristics();
+    }
+
+    // Permet la reproduction
+    public void reproduce() {
+        int numberOfYoung = new Random().nextInt(7) + 1;
+
+        // Statistiques par défaut pour un jeune lycanthrope
+        String youngRank = "γ";
+        boolean betaExists = pack.getMembers().stream().anyMatch(member -> "β".equals(member.getRank()));
+        if (!betaExists) {
+            youngRank = "β";
+        }
+
+        for (int i = 0; i < numberOfYoung; i++) {
+            char sex = new Random().nextBoolean() ? 'M' : 'F';
+            String name = "Jeune " + (i + 1);
+            // Statistiques par défaut pour un jeune lycanthrope
+            int size = 150;
+            int age = 0;
+            int strength = 5;
+            int endurance = 5;
+            int dominationFactor = 0;
+            double impulsiveness = 0.5;
+
+            Lycanthrope young = new Lycanthrope(name, sex, size, age, strength, endurance, "jeune", youngRank, dominationFactor, impulsiveness, pack, false);
+            pack.addMember(young);
+        }
+        System.out.println(numberOfYoung + " nouveau(x) lycanthrope(s) de rang " + youngRank + " sont nés du couple alpha.");
+    }
+
+    // Getters & Setters
+    public Lycanthrope getMale() {
+        return male;
+    }
+    public Lycanthrope getFemale() {
+        return female;
+    }
+}
